6d691da3f8c33da0137470472394577dbf0d5f0d
Svetlana Polozova <svetabjs@gmail.com>
2021-04-21 12:22:08 +0300

    boostrap

--- webpack.config.js
+++ webpack.config.js
@@ -19,25 +19,9 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'eslint-loader',
-      },
-      {
-        test: /\\.(js|jsx)$/,
-        loader: 'babel-loader',
-      },
-      {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/,
-        type: 'asset',
-      },
-
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
     ],
   },
 };
