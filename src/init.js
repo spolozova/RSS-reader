2972c4623bac0240d0b1ff6c95dfcd43e3c1bd00
Svetlana Polozova <svetabjs@gmail.com>
2021-04-28 17:37:45 +0300

    fix init.js

--- src/init.js
+++ src/init.js
@@ -3,6 +3,7 @@ import _ from 'lodash';
 import i18next from 'i18next';
 import axios from 'axios';
 import 'bootstrap/js/dist/modal.js';
+import 'bootstrap';
 
 import initView from './view.js';
 import resources from './locales';
