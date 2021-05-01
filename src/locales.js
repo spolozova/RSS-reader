736fd803ac43b5ca57fca43fed2f65c3d7b57457
Svetlana Polozova <svetabjs@gmail.com>
2021-05-01 09:08:33 +0300

    fix locales

--- src/locales.js
+++ src/locales.js
@@ -1,19 +1,17 @@
 export default {
-  ru: {
-    translation: {
-      invalidUrl: 'Ссылка должна быть валидным URL',
-      repeatedUrl: 'RSS уже существует',
-      parserError: 'Ресурс не содержит валидный RSS',
-      networkError: 'Ошибка сети',
-      success: 'RSS успешно загружен',
-      empty: 'Не должно быть пустым',
-      buttons: {
-        postButton: 'Просмотр',
-        modalClose: 'Закрыть',
-        modalread: 'Читать полностью',
-      },
-      feeds: 'Фиды',
-      posts: 'Посты',
+  translation: {
+    invalidUrl: 'Ссылка должна быть валидным URL',
+    repeatedUrl: 'RSS уже существует',
+    parserError: 'Ресурс не содержит валидный RSS',
+    networkError: 'Ошибка сети',
+    success: 'RSS успешно загружен',
+    empty: 'Не должно быть пустым',
+    buttons: {
+      postButton: 'Просмотр',
+      modalClose: 'Закрыть',
+      modalread: 'Читать полностью',
     },
+    feeds: 'Фиды',
+    posts: 'Посты',
   },
 };
